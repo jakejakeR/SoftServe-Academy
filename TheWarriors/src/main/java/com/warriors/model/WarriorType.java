83a27f4590f124a3b47b85059f2df6041fc911b7
Jakub Rogowski <jakerogowski33gmail.com>
2022-07-29 09:26:28 +0200

    Defender added to Warrior Factory

--- TheWarriors/src/main/java/com/warriors/model/WarriorType.java
+++ TheWarriors/src/main/java/com/warriors/model/WarriorType.java
@@ -2,10 +2,14 @@ package com.warriors.model;
 
 import java.util.function.Supplier;
 
+/**
+ * Warrior Factory
+ */
 public enum WarriorType {
 
     WARRIOR(Warrior::new),
-    KNIGHT(Knight::new);
+    KNIGHT(Knight::new),
+    DEFENDER(Defender::new);
 
     private final Supplier<Unit> constructor;
 
