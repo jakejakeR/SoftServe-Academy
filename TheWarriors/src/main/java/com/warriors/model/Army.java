805874bcb72f4f2d1ed3b7abb82d63a31a0ab1e1
Jakub Rogowski <jakerogowski33@gmail.com>
2022-07-30 23:15:28 +0200

    Added Iterator to Army class

--- TheWarriors/src/main/java/com/warriors/model/Army.java
+++ TheWarriors/src/main/java/com/warriors/model/Army.java
@@ -3,8 +3,7 @@ package com.warriors.model;
 import com.warriors.model.warriors.Warrior;
 import com.warriors.model.warriors.interfaces.Fightable;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 import java.util.function.Supplier;
 
 /**
@@ -13,10 +12,46 @@ import java.util.function.Supplier;
 public class Army {
     private final Queue<Fightable> troops = new LinkedList<>();
 
-    // Factory method pattern
+    /* Iterator that always returns the first alive warrior,
+    not removes dead warriors, because maybe in future
+    they will be required to revive */
+    List<Fightable> troopsList = new ArrayList<>();
+
+    Iterator<Fightable> firstAlive() {
+        return new FirstAliveIterator();
+    }
+
+    private class FirstAliveIterator implements Iterator<Fightable> {
+        int cursor = 0;
+
+        /**
+         * If the current warrior is not alive -> skip him (cursor++)
+         * If the current warrior is alive -> exit loop
+         * Iterator points to the first alive warrior
+         * @return true if next warrior is alive
+         */
+        @Override
+        public boolean hasNext() {
+            while (cursor < troopsList.size() && !troopsList.get(cursor).isAlive()) {
+                cursor++;
+            }
+            return cursor < troopsList.size();
+        }
+
+        @Override
+        public Fightable next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return troopsList.get(cursor); // Typically, next() should return something and move to the next element
+        }
+    }
+
+
+    // Factory method pattern (adding warriors to troopsCollection!!!)
     public Army addUnits(WarriorType warriorType, int quantity) {
         for (int i = 0; i < quantity; i++) {
-            troops.add(warriorType.getConstructor().get());
+            troopsList.add(warriorType.getConstructor().get());
         }
         return this;
     }
