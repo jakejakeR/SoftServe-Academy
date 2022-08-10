4afa32709ec11686ed2b83a8918250a8f497169f
Jakub Rogowski <jakerogowski33gmail.com>
2022-08-10 09:40:34 +0200

    folders rename v2

--- the-warriors/src/test/java/com/warriors/service/FightTestSuite.java
+++ the-warriors/src/test/java/com/warriors/service/FightTestSuite.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-class FightTestSuite {
+class FightSuiteTest {
 
     @Test
         // 1. Fight
