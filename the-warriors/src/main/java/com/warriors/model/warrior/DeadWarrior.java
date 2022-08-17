485544bd1961c64b469b81ebc864fe45c1fc80c5
Jakub Rogowski <jakerogowski33gmail.com>
2022-08-17 16:19:21 +0200

    DeadWarrior changed, extends now Warrior

--- the-warriors/src/main/java/com/warriors/model/warrior/DeadWarrior.java
+++ the-warriors/src/main/java/com/warriors/model/warrior/DeadWarrior.java
@@ -1,43 +1,19 @@
 package com.warriors.model.warrior;
 
-import com.warriors.command.ICommand;
 import com.warriors.model.equipment.Equipment;
 import com.warriors.model.warrior.interfaces.IWarrior;
-import com.warriors.model.warrior.interfaces.observer.Observer;
 
-import java.util.Optional;
-
-public class DeadWarrior implements IWarrior {
+public class DeadWarrior extends Warrior {
     public static final int INITIAL_HEALTH = 1;
     public static final int INITIAL_ATTACK = 3;
     private int health;
     private final IWarrior decoratedWarrior;
 
     public DeadWarrior(IWarrior deadWarrior) {
+        super(INITIAL_HEALTH, INITIAL_ATTACK);
         deadWarrior.dropEquipment();
         this.decoratedWarrior = deadWarrior;
         this.health = INITIAL_HEALTH;
-    }
-
-
-    @Override
-    public int getAttack() {
-        return INITIAL_ATTACK;
-    }
-
-    @Override
-    public int getHealth() {
-        return health;
-    }
-
-    @Override
-    public int getInitialHealth() {
-        return INITIAL_HEALTH;
-    }
-
-    @Override
-    public void setHealth(int health) {
-        decoratedWarrior.setHealth(health);
     }
 
     @Override
@@ -46,32 +22,12 @@ public class DeadWarrior implements IWarrior {
     }
 
     @Override
-    public Optional<IWarrior> getNextBehind() {
-        return decoratedWarrior.getNextBehind();
+    public int getAttack() {
+        return INITIAL_ATTACK + getEquipment().getAttackModifiers();
     }
 
     @Override
-    public void setNextBehind(IWarrior nextBehind) {
-        decoratedWarrior.setNextBehind(nextBehind);
-    }
-
-    @Override
-    public void processCommand(ICommand command, IWarrior sender) {
-        decoratedWarrior.processCommand(command, sender);
-    }
-
-    @Override
-    public String toString() {
-        return this.getClass().getSimpleName() + " {HP:" + getHealth() + "| A:" + getAttack() + "}";
-    }
-
-    @Override
-    public void registerObserver(Observer observer) {
-        decoratedWarrior.registerObserver(observer);
-    }
-
-    @Override
-    public void notifyObserver() {
-        decoratedWarrior.notifyObserver();
+    public int getInitialHealth() {
+        return INITIAL_HEALTH + getEquipment().getHealthModifiers();
     }
 }
--- the-warriors/src/test/java/com/warriors/model/DeadWarriorTest.java
+++ the-warriors/src/test/java/com/warriors/model/DeadWarriorTest.java
@@ -1,4 +1,43 @@
 package com.warriors.model;
 
-public class DeadWarriorTest {
+import com.warriors.model.equipment.Forge;
+import com.warriors.model.warrior.DeadWarrior;
+import com.warriors.model.warrior.Healer;
+import com.warriors.model.warrior.Lancer;
+import com.warriors.service.Battle;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeadWarriorTest {
+
+    @Test
+    void givenDeadHealerWithMagicWand_whenTurnedIntoDeadWarrior_thenDoesNotHaveHealerSpecifications() {
+
+        // given
+        var kaputHealer = new Healer();
+        kaputHealer.equipWeapon(Forge.forgeMagicWand());
+        kaputHealer.setHealth(0);
+
+        // when
+        var deadWarriorOfHealer = new DeadWarrior(kaputHealer);
+
+        // then
+        assertFalse(kaputHealer.isAlive());
+        assertEquals(3, deadWarriorOfHealer.getAttack());
+        assertEquals(1, deadWarriorOfHealer.getHealth());
+        assertEquals(1, deadWarriorOfHealer.getInitialHealth());
+        assertTrue(deadWarriorOfHealer.getEquipment().isEmpty());
+    }
+
+    @Test
+    void givenArmyOfDeadWarriors_whenFightAgainstArmyOfOneLancer_thenDeadShouldPassPierceCommand() {
+        // given
+        var deadArmy = new Army();
+        deadArmy.addUnits(() -> new DeadWarrior(new Healer()), 10);
+        var lancerArmy = new Army().addUnits(Lancer::new, 1);
+
+        // when
+        Battle.fight(deadArmy, lancerArmy);
+    }
 }
